4571f732b17eea2852c7fb786de388fde90fffae
arturzaczek1 <artur.zaczek@tlen.pl>
2019-03-31 15:13:21 +0200

    added modal window to confirm delete post

--- src/main/java/pl/arturzaczek/school/controllers/PostController.java
+++ src/main/java/pl/arturzaczek/school/controllers/PostController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import pl.arturzaczek.school.entitis.Post;
 import pl.arturzaczek.school.form.PostRegisterForm;
 import pl.arturzaczek.school.services.PostService;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -21,28 +23,32 @@ public class PostController {
     }
 
 
-    @GetMapping ("post/register")
-    public String getNewPostForm (Model model){
+    @GetMapping("post/register")
+    public String getNewPostForm(Model model) {
         model.addAttribute("postRegisterForm", new PostRegisterForm());
         return "post/new-post-form";
     }
-    @PostMapping ("post/register/new")
-    public String registerPost (@ModelAttribute @Valid PostRegisterForm postRegisterForm, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
+
+    @PostMapping("post/register/new")
+    public String registerPost(@ModelAttribute @Valid PostRegisterForm postRegisterForm, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             model.addAttribute("postRegisterForm", postRegisterForm);
             return "post/new-post-form";
         }
         postService.saveNewPost(postRegisterForm);
+        model.addAttribute("postList", postService.getPostList());
         return "/post/post-management";
     }
-    @GetMapping ("post/management")
-    public String goToPostManagement (Model model){
-        model.addAttribute("posts", postService.getPostList());
+
+    @GetMapping("post/management")
+    public String goToPostManagement(Model model) {
+        model.addAttribute("postList", postService.getPostList());
         return "/post/post-management";
     }
-    @GetMapping ("/post/delete/{id}")
-    public String deletePost (@PathVariable String id){
-        long longId = Long.parseLong(id);
+
+    @PostMapping ("/post/delete/")
+    public String deletePost2(@RequestParam String post_id ) {
+        long longId = Long.parseLong(post_id);
         postService.deleteOnePost(longId);
         return "/post/post-management";
     }
