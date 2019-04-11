350b7080b3825a0a3f0993dc2841d59c263559cb
arturzaczek1 <artur.zaczek@tlen.pl>
2019-04-11 17:02:11 +0200

    connection student with school class

--- src/main/java/pl/arturzaczek/school/controllers/StudentController.java
+++ src/main/java/pl/arturzaczek/school/controllers/StudentController.java
@@ -7,7 +7,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.arturzaczek.school.forms.StudentForm;
+import pl.arturzaczek.school.services.SchoolClassService;
 import pl.arturzaczek.school.services.StudentService;
 
 import javax.validation.Valid;
@@ -16,15 +18,18 @@ import javax.validation.Valid;
 public class StudentController {
 
     private StudentService studentService;
+    private SchoolClassService schoolClassService;
 
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, SchoolClassService schoolClassService) {
         this.studentService = studentService;
+        this.schoolClassService = schoolClassService;
     }
 
     @GetMapping("/student/management")
     public String goToStudentManagement(Model model) {
         model.addAttribute("students", studentService.getStudentsList());
+        model.addAttribute("classes", schoolClassService.getAllSC());
         return "student/students-management";
     }
 
@@ -44,6 +49,14 @@ public class StudentController {
         String info = studentService.registerStudent(studentform);
         model.addAttribute("students", studentService.getStudentsList());
         model.addAttribute("info", info);
+        return "student/students-management";
+    }
+
+    @PostMapping("/student/s-c/add")
+    public String connectStudentWithSchoolClass(@RequestParam String add_class, @RequestParam String student_id, Model model) {
+        String info = studentService.connectWithSchoolClass(student_id, add_class);
+        model.addAttribute("info", info);
+        model.addAttribute("classes", schoolClassService.getAllSC());
         return "student/students-management";
     }
 }
--- src/main/java/pl/arturzaczek/school/services/SchoolClassService.java
+++ src/main/java/pl/arturzaczek/school/services/SchoolClassService.java
@@ -3,14 +3,13 @@ package pl.arturzaczek.school.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.arturzaczek.school.entitis.SchoolClass;
-import pl.arturzaczek.school.entitis.User;
 import pl.arturzaczek.school.forms.SchoolClassRegisterForm;
 import pl.arturzaczek.school.repositories.SchoolClassRepository;
 import pl.arturzaczek.school.repositories.UserRepository;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 @Service
 public class SchoolClassService {
